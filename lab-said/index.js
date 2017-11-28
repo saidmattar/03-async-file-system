c0af2a55df3a028a076ba47f913121c8d22741a5
saidmattar <ssabdou@gmail.com>
2017-11-27 21:42:49 -0800

    added file four and five

--- lab-said/index.js
+++ lab-said/index.js
@@ -7,4 +7,5 @@ readFile(`${__dirname}/assets`, (err,data) =>{
 });
 
 console.log('said');
+
 console.log('to get git to store yor credintials type "git config --global credential.helper wincred" into your command line');
