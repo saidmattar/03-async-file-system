b30358f1f071427b1d6a57a01721d328d07e31bb
saidmattar <ssabdou@gmail.com>
2017-11-27 21:38:24 -0800

    re coded my start pint and my read-file.js to be dynamic regardless of the number of text files

--- lab-said/index.js
+++ lab-said/index.js
@@ -1,3 +1,10 @@
 'use strict';
+
+const readFile = require('./lib/read-file');
+readFile(`${__dirname}/assets`, (err,data) =>{
+  if(err) console.error(err);
+  console.log(data);
+});
+
 console.log('said');
 console.log('to get git to store yor credintials type "git config --global credential.helper wincred" into your command line');
--- lab-said/lib/read-file.js
+++ lab-said/lib/read-file.js
@@ -2,29 +2,23 @@
 
 const fs = require('fs');
 
-let vegies = module.exports = callback => {
-  let first, second, third;
+module.exports = (dirPath, callback) => {
+  let results = {};
 
-  fs.vegies(`${__dirname}/../assets/one.txt`, (err, oneData) => {
-    if(err) callback(new Error('error'));
-    first = oneData.toString('hex', 0, 8);
-
-    fs.vegies(`${__dirname}/../assets/two.txt`, (err, twoData) => {
-      if(err) callback(new Error('error'));
-      second = twoData.toString('hex', 0, 8);
-
-
-      fs.vegies(`${__dirname}/../assets/three.txt`, (err, threeData) => {
-        if(err) callback(new Error('error'));
-        third = threeData.toString('hex', 0, 8);
-
-        callback(null,first, second, third);
-      });
-    });
+  fs.readdir(dirPath, (err, files) => {
+    if(err) callback(err);
+    readFiles(files);
   });
-};
 
-vegies((err,assets) => {
-  if(err) console.error(err);
-  console.log(assets);
-});
+  function readFiles(filesArr) {
+    if(!filesArr.length) {
+      callback(null, results);
+      return;
+    }
+
+    fs.readFile(`${dirPath}/${filesArr.pop()}`, (err, data) => {
+      results[filesArr.length] = data.toString('hex', 0, 16);
+      readFiles(filesArr);
+    });
+  }
+};
